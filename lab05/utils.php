35d3837caa01396b568c1a2c4de3aededb9b7ed2
kovfanni004 <126889015+kovfanni004@users.noreply.github.com>
2024-04-07 08:50:27 +0200

    Add files via upload

---
+++ lab05/utils.php
@@ -0,0 +1,14 @@
+<?php
+
+// Utility funciton for creating HTML select elements
+function generateNumericSelect($name, $from, $to)
+{
+    echo "<select name=\"$name\" class=\"form-select\">";
+    for($i=$from;$i<=$to;$i++)
+    {
+        echo "<option value=\"$i\">$i</option>";
+    }
+    echo "</select>";
+}
+
+?>
